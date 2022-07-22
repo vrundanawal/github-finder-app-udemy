d44dfe24da7e093d44b9fe39818cab052eae6c25
Vrunda Nawal <2190013@cognizant.com>
2022-07-22 12:20:25 +0100

    Setup the files with Tailwind css and Daisy Ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-purple-500">
+      <h1 className="text-3xl font-bold underline">Hello from App</h1>
+      <button className="btn">Click Me</button>
     </div>
   );
 }
-
 export default App;
