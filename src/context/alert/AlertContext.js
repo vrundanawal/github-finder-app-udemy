92ae6b01b938a8698dcada4b5ca7500ea041387e
Vrunda Nawal <2190013@cognizant.com>
2022-07-25 15:32:05 +0100

    alert context and reducer added

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,35 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ childern }) => {
+  //set initial setup
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  //set an Alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { msg, type },
+    });
+
+    setTimeout(
+      () =>
+        dispatch({
+          type: "REMOVE_ALERT",
+        }),
+      3000
+    );
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {childern}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertContext;
