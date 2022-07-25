1a7dba7f5fe435519a261bd31a6989d390d01b95
Vrunda Nawal <2190013@cognizant.com>
2022-07-25 08:58:07 +0100

    UserItem component to display a single userItem created

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -21,7 +22,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => {
-          return <h3 key={user.id}>{user.login}</h3>;
+          return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
