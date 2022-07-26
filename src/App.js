9e9f377dac60852b7164b2979999a122539937f5
Vrunda Nawal <2190013@cognizant.com>
2022-07-26 09:29:23 +0100

    user profile data show to the page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
 
-            <main className="container mx-auto px-3 pb-12">
+            <main className="container mx-auto px-3 pb-12 ">
               <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
