cd8a753106bbaaf417ef9cf73d15789059ebaddd
Vrunda Nawal <2190013@cognizant.com>
2022-07-26 12:03:37 +0100

    Repos item coponent show all the details

--- src/components/repos/RepoList.js
+++ src/components/repos/RepoList.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import RepoItem from "./RepoItem";
 
 const RepoList = ({ repos }) => {
   return (
@@ -8,7 +9,7 @@ const RepoList = ({ repos }) => {
           Latest Respositories
         </h2>
         {repos.map((repo) => (
-          <h3>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
--- src/pages/User.js
+++ src/pages/User.js
@@ -14,6 +14,7 @@ const User = () => {
   useEffect(() => {
     getUser(params.login);
     getUserRepos(params.login);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   //object destructuring
