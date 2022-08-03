45275306a13d7ec6f74fe42eff406b70901fbe72
Vrunda Nawal <vrunda.nawal@cognizant.com>
2022-08-03 12:14:02 +0100

    Remove the unnecessory comment and make it clean code

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -11,36 +11,9 @@ export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  //   const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-  //   const { items } = await response.json();
-  //   return items;
   const response = await github.get(`/search/users?${params}`);
   return response.data.items;
 };
-/*
-//get a single user
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`);
-  //added some validation if url is wrong
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-    return data;
-  }
-};
-
-//get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  });
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`);
-  const data = await response.json();
-  return data;
-};
-*/
 
 //create one single function which is going to call single user and user repos
 //get user and repos
